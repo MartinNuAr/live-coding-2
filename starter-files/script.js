60e89bc58c3d3f4a9bfd0367a1b23a1f2bc9833c
Juan Martín Núñez Aréchiga <jmartin.nuar@gmail.com>
2022-09-29 23:08:01 -0500

    Ejercicio resuelto

--- starter-files/script.js
+++ starter-files/script.js
@@ -1,6 +1,35 @@
 // API
 const API_ENDPOINT = 'https://yesno.wtf/api';
 
+// Se definen los campos del HTML
+
+let ans = document.getElementById("answer");
+let btn = document.getElementById("button");
+let inputField = document.getElementById("input");
+let error = document.getElementById("error");
+
+const fetchAnswer = () => {
+    fetch(API_ENDPOINT)
+    .then(data => data.json())
+    .then(data => showAnswer(data.answer))
+}
+
+const showAnswer = answer => {
+    ans.innerHTML = `<p>${answer}</p>` // Función para añadir una etiqueta de parrafo con Yes/No
+}
+
+btn.addEventListener("click", function(event){
+    event.preventDefault();
+    if (inputField.value == null || inputField.value == ""){ // Validación para campo vacío
+        error.innerHTML = "Please write something in the field";
+    } else {
+        error.innerHTML = ""; // Borra el mensaje de error
+        fetchAnswer();
+        inputField.value = ""; // Borra el campo para la respuesta
+        setTimeout(function(){ans.innerHTML=""}, 5000); // Borra la respuesta en 5 segundos
+    }
+})
+
 /**
  * STEPS:
  *
